d50a6d08e71c4cc20353b1e6aeda3939e50630ed
jlbeck <jlbeck@mail.bradley.edu>
2022-10-05 15:07:45 -0500

    Finished Assignment 5A

---
+++ PennyPixel_2DTilemapProject/Assets/CollectableGem/Scripts/DisplayScore.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DisplayScore : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    public Text textbox;
+
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //If the game is not over, display score
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
+
+        if (gameOver)
+        {
+            textbox.text = "You win!";
+        }
+
+    }
+}
